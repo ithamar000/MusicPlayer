74f889c0b3dceb895496e41b8ecbe8e6e16668d7
Itamar Yacoby <ithamar000@yahoo.com>
2021-04-08 15:23:07 +0300

    glide and cell

---
+++ app/src/main/java/com/example/musicplayer/trackAdapter.java
@@ -0,0 +1,67 @@
+package com.example.musicplayer;
+
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class trackAdapter extends RecyclerView.Adapter<trackAdapter.TrackViewHolder> {
+
+    private List<Track> trackList;
+    private Context context;
+
+    public trackAdapter(List<Track> trackList, Context context) {
+        this.trackList = trackList;
+        this.context = context;
+    }
+
+    public class TrackViewHolder extends RecyclerView.ViewHolder{
+        ImageView trackImageIv;
+        TextView trackTitleTv;
+        TextView trackArtistTv;
+
+        public TrackViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            trackImageIv = itemView.findViewById(R.id.track_cell_image);
+            trackTitleTv = itemView.findViewById(R.id.track_cell_title);
+            trackArtistTv = itemView.findViewById(R.id.track_cell_artist);
+        }
+    }
+
+    @NonNull
+    @Override
+    public TrackViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.track_cell,parent,false);
+        TrackViewHolder trackViewHolder = new TrackViewHolder(view);
+        return trackViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TrackViewHolder holder, int position) {
+
+        Track track = trackList.get(position);
+        holder.trackArtistTv.setText(track.getArtist());
+        holder.trackTitleTv.setText(track.getTitle());
+        Glide.with(context).load(track.getPicLink()).into(holder.trackImageIv);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return trackList.size();
+    }
+}
